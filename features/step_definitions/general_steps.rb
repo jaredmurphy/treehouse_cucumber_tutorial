1a960109db18254a1a699b99b2cb4789bcffb547
Jared <jaredcmurphy@gmail.com>
2016-04-25 14:55:51 -0400

    adds general_steps.rb and path_steps.rb

---
+++ features/step_definitions/general_steps.rb
@@ -0,0 +1,22 @@
+Given(/^I am a the home page$/) do
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^I follow "([^"]*)"$/) do |arg1|
+pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
+pending # Write code here that turns the phrase above into concrete
+actions
+end
+
+When(/^I press "([^"]*)"$/) do |arg1|
+pending # Write code here that turns the phrase above into concrete
+actions
+end
+
+Then(/^I should see "([^"]*)"$/) do |arg1|
+pending # Write code here that turns the phrase above into concrete
+actions
+end
